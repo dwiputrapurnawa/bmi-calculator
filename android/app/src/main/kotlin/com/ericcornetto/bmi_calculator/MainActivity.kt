a1a323118eebb07fe37bde56f1dfda3fbbb80798
idabagusdwiputrapurnawa <putrapurnawa@gmail.com>
2022-03-30 16:50:39 +0800

    initial commit

---
+++ android/app/src/main/kotlin/com/ericcornetto/bmi_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ericcornetto.bmi_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'input_page.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  const BMICalculator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: InputPage(),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Color(0xFF090D22),
+        appBarTheme: AppBarTheme(color: Color(0xFF0A0E21)),
+      ),
+    );
+  }
+}
+
